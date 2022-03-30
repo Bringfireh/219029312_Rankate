92a6ed13f73f3508b6457119ff38bc6e05926688
Bringfireh <ngutortsenongo@gmail.com>
2022-03-30 09:43:44 +0100

    Add project files.

---
+++ 219029312_Rankate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _219029312_Rankate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
